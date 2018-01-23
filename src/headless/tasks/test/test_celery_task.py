777b87149c2fec3fd41a667e1fc48f1672944845
Ismail Sunni <imajimatika@gmail.com>
2018-01-23 17:48:59 +0700

    Add more data to test.

--- src/headless/tasks/test/test_celery_task.py
+++ src/headless/tasks/test/test_celery_task.py
@@ -5,8 +5,11 @@ import os
 import unittest
 from headless.tasks.inasafe_wrapper import get_keywords
 
-from safe.definitions.layer_purposes import layer_purpose_exposure
+from safe.definitions.layer_purposes import (
+    layer_purpose_exposure, layer_purpose_hazard)
 from safe.definitions.exposure import exposure_place
+from safe.definitions.hazard import hazard_earthquake
+from safe.definitions.extra_keywords import extra_keyword_time_zone
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -25,3 +28,14 @@ class TestHeadlessCeleryTask(unittest.TestCase):
         self.assertEqual(
             keywords['layer_purpose'], layer_purpose_exposure['key'])
         self.assertEqual(keywords['exposure'], exposure_place['key'])
+
+        layer_path = os.path.join(dir_path, 'data', 'earthquake.asc')
+        self.assertTrue(os.path.exists(layer_path))
+        result = get_keywords.delay(layer_path)
+        keywords = result.get()
+        self.assertIsNotNone(keywords)
+        self.assertEqual(
+            keywords['layer_purpose'], layer_purpose_hazard['key'])
+        self.assertEqual(keywords['hazard'], hazard_earthquake['key'])
+        time_zone = keywords['extra_keywords'][extra_keyword_time_zone['key']]
+        self.assertEqual(time_zone, 'Asia/Jakarta')
