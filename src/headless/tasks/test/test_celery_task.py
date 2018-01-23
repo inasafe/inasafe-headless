146d0560eca2d6a337b700c72ad68542c7c9c9f8
Ismail Sunni <imajimatika@gmail.com>
2018-01-23 20:09:15 +0700

    Add run analysis task.

--- src/headless/tasks/test/test_celery_task.py
+++ src/headless/tasks/test/test_celery_task.py
@@ -3,13 +3,14 @@
 
 import os
 import unittest
-from headless.tasks.inasafe_wrapper import get_keywords
+from headless.tasks.inasafe_wrapper import get_keywords, run_analysis
 
 from safe.definitions.layer_purposes import (
-    layer_purpose_exposure, layer_purpose_hazard)
+    layer_purpose_exposure, layer_purpose_hazard, layer_purpose_aggregation)
 from safe.definitions.exposure import exposure_place
 from safe.definitions.hazard import hazard_earthquake
 from safe.definitions.extra_keywords import extra_keyword_time_zone
+from safe.definitions.constants import ANALYSIS_SUCCESS
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -39,3 +40,47 @@ class TestHeadlessCeleryTask(unittest.TestCase):
         self.assertEqual(keywords['hazard'], hazard_earthquake['key'])
         time_zone = keywords['extra_keywords'][extra_keyword_time_zone['key']]
         self.assertEqual(time_zone, 'Asia/Jakarta')
+
+        layer_path = os.path.join(dir_path, 'data', 'small_grid.geojson')
+        self.assertTrue(os.path.exists(layer_path))
+        result = get_keywords.delay(layer_path)
+        keywords = result.get()
+        self.assertIsNotNone(keywords)
+        self.assertEqual(
+            keywords['layer_purpose'], layer_purpose_aggregation['key'])
+
+    def test_run_analysis(self):
+        """Test run analysis synchronously."""
+        # Layers
+        exposure_uri = os.path.join(dir_path, 'data', 'places.geojson')
+        hazard_uri = os.path.join(dir_path, 'data', 'earthquake.asc')
+        aggregation_uri = os.path.join(dir_path, 'data', 'small_grid.geojson')
+
+        # With aggregation
+        result = run_analysis(hazard_uri, exposure_uri, aggregation_uri)
+        self.assertEqual(ANALYSIS_SUCCESS, result['status'])
+        self.assertLess(0, len(result['output']))
+
+        # No aggregation
+        result = run_analysis(hazard_uri, exposure_uri)
+        self.assertEqual(ANALYSIS_SUCCESS, result['status'])
+        self.assertLess(0, len(result['output']))
+
+    def test_run_analysis_async(self):
+        """Test run analysis asynchronously."""
+        # Layers
+        exposure_uri = os.path.join(dir_path, 'data', 'places.geojson')
+        hazard_uri = os.path.join(dir_path, 'data', 'earthquake.asc')
+        aggregation_uri = os.path.join(dir_path, 'data', 'small_grid.geojson')
+
+        # With aggregation
+        result = run_analysis.delay(hazard_uri, exposure_uri, aggregation_uri)
+        result_dict = result.get()
+        self.assertEqual(ANALYSIS_SUCCESS, result_dict['status'])
+        self.assertLess(0, len(result_dict['output']))
+
+        # No aggregation
+        result = run_analysis.delay(hazard_uri, exposure_uri)
+        result_dict = result.get()
+        self.assertEqual(ANALYSIS_SUCCESS, result_dict['status'])
+        self.assertLess(0, len(result_dict['output']))
