25982f1bc27ae9efd2ab9f6366cb5b957c93e929
Ismail Sunni <imajimatika@gmail.com>
2018-01-24 23:33:57 +0700

    Add multi exposure analysis task.

--- src/headless/tasks/inasafe_wrapper.py
+++ src/headless/tasks/inasafe_wrapper.py
@@ -7,6 +7,8 @@ from headless.celery_app import app
 from qgis.core import QgsCoordinateReferenceSystem
 from safe.utilities.metadata import read_iso19115_metadata
 from safe.impact_function.impact_function import ImpactFunction
+from safe.impact_function.multi_exposure_wrapper import (
+    MultiExposureImpactFunction)
 from safe.gis.tools import load_layer
 from safe.gis.raster.contour import create_smooth_contour
 from safe.definitions.constants import PREPARE_SUCCESS, ANALYSIS_SUCCESS
@@ -57,7 +59,8 @@ def run_analysis(
     :param crs: CRS for the analysis (if the aggregation is not set).
     :param crs: QgsCoordinateReferenceSystem
 
-    :returns: A dictionary of output's layer key and Uri.
+    :returns: A dictionary of output's layer key and Uri with status and
+        message.
     :rtype: dict
     """
     impact_function = ImpactFunction()
@@ -76,6 +79,72 @@ def run_analysis(
         status, message = impact_function.run()
         if status == ANALYSIS_SUCCESS:
             outputs = impact_function.outputs
+            output_dict = {}
+            for layer in outputs:
+                output_dict[layer.keywords['layer_purpose']] = layer.source()
+
+            return {
+                'status': ANALYSIS_SUCCESS,
+                'message': '',
+                'output': output_dict
+            }
+        else:
+            LOGGER.debug('Analysis failed %s' % message)
+            return {
+                'status': status,
+                'message': message,
+                'output': {}
+            }
+    else:
+        LOGGER.debug('Impact function is not ready: %s' % prepare_message)
+        return {
+            'status': prepare_status,
+            'message': prepare_message,
+            'output': {}
+        }
+
+
+@app.task(queue='inasafe-headless')
+def run_multi_exposure_analysis(
+        hazard_layer_uri,
+        exposure_layer_uris,
+        aggregation_layer_uri=None,
+        crs=None
+):
+    """Run analysis for multi exposure.
+
+    :param hazard_layer_uri: Uri to hazard layer.
+    :type hazard_layer_uri: basestring
+
+    :param exposure_layer_uris: List of uri to exposure layers.
+    :type exposure_layer_uris: list
+
+    :param aggregation_layer_uri: Uri to aggregation layer.
+    :type aggregation_layer_uri: basestring
+
+    :param crs: CRS for the analysis (if the aggregation is not set).
+    :param crs: QgsCoordinateReferenceSystem
+
+    :returns: A dictionary of output's layer key and Uri with status and
+        message.
+    :rtype: dict
+    """
+    multi_exposure_if = MultiExposureImpactFunction()
+    multi_exposure_if.hazard = load_layer(hazard_layer_uri)[0]
+    exposures = [load_layer(layer_uri)[0] for layer_uri in exposure_layer_uris]
+    multi_exposure_if.exposures = exposures
+    if aggregation_layer_uri:
+        multi_exposure_if.aggregation = load_layer(aggregation_layer_uri)[0]
+    elif crs:
+        multi_exposure_if.crs = crs
+    else:
+        multi_exposure_if.crs = QgsCoordinateReferenceSystem(4326)
+    prepare_status, prepare_message = multi_exposure_if.prepare()
+    if prepare_status == PREPARE_SUCCESS:
+        LOGGER.debug('Multi exposure function is ready')
+        status, message = multi_exposure_if.run()
+        if status == ANALYSIS_SUCCESS:
+            outputs = multi_exposure_if.outputs
             output_dict = {}
             for layer in outputs:
                 output_dict[layer.keywords['layer_purpose']] = layer.source()
