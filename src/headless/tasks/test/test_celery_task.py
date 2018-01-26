c181834b06aefaa57189dfcd303cd700d2e04938
Ismail Sunni <imajimatika@gmail.com>
2018-01-26 10:04:44 +0700

    Add generate report for analysis.

--- src/headless/tasks/test/test_celery_task.py
+++ src/headless/tasks/test/test_celery_task.py
@@ -7,16 +7,22 @@ from headless.tasks.inasafe_wrapper import (
     get_keywords,
     run_analysis,
     generate_contour,
-    run_multi_exposure_analysis
+    run_multi_exposure_analysis,
+    generate_report,
 )
 from headless.settings import OUTPUT_DIRECTORY
 
 from safe.definitions.layer_purposes import (
-    layer_purpose_exposure, layer_purpose_hazard, layer_purpose_aggregation)
+    layer_purpose_exposure,
+    layer_purpose_hazard,
+    layer_purpose_aggregation,
+    layer_purpose_exposure_summary,
+)
 from safe.definitions.exposure import exposure_place
 from safe.definitions.hazard import hazard_earthquake
 from safe.definitions.extra_keywords import extra_keyword_time_zone
 from safe.definitions.constants import ANALYSIS_SUCCESS
+from safe.report.impact_report import ImpactReport
 
 __copyright__ = "Copyright 2018, The InaSAFE Project"
 __license__ = "GPL version 3"
@@ -140,7 +146,7 @@ class TestHeadlessCeleryTask(unittest.TestCase):
         # of exposures
         self.assertEqual(num_exposure_output, len(exposure_layer_uris))
 
-    def test_generate_contour_async(self):
+    def test_generate_contour(self):
         """Test generate_contour task."""
         # Layer
         result_delay = generate_contour.delay(shakemap_layer_uri)
@@ -148,3 +154,30 @@ class TestHeadlessCeleryTask(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertTrue(os.path.exists(result))
         self.assertTrue(result.startswith(OUTPUT_DIRECTORY))
+
+    def test_generate_report(self):
+        """Test generate report for single analysis."""
+        # Run analysis first
+        result_delay = run_analysis.delay(
+            earthquake_layer_uri, place_layer_uri, aggregation_layer_uri)
+        result = result_delay.get()
+        self.assertEqual(ANALYSIS_SUCCESS, result['status'])
+        self.assertLess(0, len(result['output']))
+        for key, layer_uri in result['output'].items():
+            self.assertTrue(os.path.exists(layer_uri))
+            self.assertTrue(layer_uri.startswith(OUTPUT_DIRECTORY))
+
+        # Retrieve impact analysis uri
+        impact_analysis_uri = result['output'][
+            layer_purpose_exposure_summary['key']]
+
+        # Generate reports
+        async_result = generate_report.delay(impact_analysis_uri)
+        result = async_result.get()
+        self.assertEqual(
+            ImpactReport.REPORT_GENERATION_SUCCESS, result['status'])
+        for key, products in result['output'].items():
+            for product_key, product_uri in products.items():
+                message = 'Product %s is not found in %s' % (
+                    product_key, product_uri)
+                self.assertTrue(os.path.exists(product_uri), message)
