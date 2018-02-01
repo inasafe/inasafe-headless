b3dd693ccf997a0b23daaedfe8702a3c93470ff3
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-02-01 15:32:53 +0700

    Improve orchestration and include production grade service

---
+++ deployment/production/docker/headless/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if [ -z "$REPO_NAME" ]; then
+	REPO_NAME=inasafe
+fi
+
+if [ -z "$IMAGE_NAME" ]; then
+	IMAGE_NAME=headless_processor
+fi
+
+if [ -z "$TAG_NAME" ]; then
+	TAG_NAME=latest
+fi
+
+if [ -z "$BUILD_ARGS" ]; then
+	BUILD_ARGS="--pull --no-cache"
+fi
+
+# Build Args Environment
+
+if [ -z "$INASAFE_CORE_TAG" ]; then
+	INASAFE_CORE_TAG=develop
+fi
+
+if [ -z "$INASAFE_HEADLESS_TAG" ]; then
+	INASAFE_HEADLESS_TAG=develop
+fi
+
+echo "INASAFE_CORE_TAG=${INASAFE_CORE_TAG}"
+echo "INASAFE_HEADLESS_TAG=${INASAFE_HEADLESS_TAG}"
+
+echo "Build: $REPO_NAME/$IMAGE_NAME:$TAG_NAME"
+echo "Build Args: $BUILD_ARGS"
+
+docker build -t ${REPO_NAME}/${IMAGE_NAME} ${BUILD_ARGS} .
+docker tag ${REPO_NAME}/${IMAGE_NAME} ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
+docker push ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
