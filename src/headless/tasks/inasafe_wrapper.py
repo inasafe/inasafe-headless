fd79e2b99c77192f4080b8855af01786fd03ad94
myarjunar <myarjunar@gmail.com>
2018-02-08 11:56:30 +0700

    apply custom layer order for map report

--- src/headless/tasks/inasafe_wrapper.py
+++ src/headless/tasks/inasafe_wrapper.py
@@ -289,7 +289,10 @@ def run_multi_exposure_analysis(
 
 @app.task(
     name='inasafe.headless.tasks.generate_report', queue='inasafe-headless')
-def generate_report(impact_layer_uri, custom_report_template_uri=None):
+def generate_report(
+        impact_layer_uri,
+        custom_report_template_uri=None,
+        custom_layer_order=None):
     """Generate report based on impact layer uri.
 
     :param impact_layer_uri: The uri to impact layer (one of them).
@@ -297,6 +300,9 @@ def generate_report(impact_layer_uri, custom_report_template_uri=None):
 
     :param custom_report_template_uri: The uri to report template.
     :type custom_report_template_uri: basestring
+
+    :param custom_layer_order: List of layers uri for map report layers order.
+    :type custom_layer_order: list
 
     :returns: A dictionary of output's report key and Uri with status and
         message.
@@ -376,7 +382,8 @@ def generate_report(impact_layer_uri, custom_report_template_uri=None):
                 map_report, custom_report_template_uri))
 
     error_code, message = (
-        impact_function.generate_report(generated_components))
+        impact_function.generate_report(
+            generated_components, ordered_layers=custom_layer_order))
     return {
         'status': error_code,
         'message': message.to_text(),
